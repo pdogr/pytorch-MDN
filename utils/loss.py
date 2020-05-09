c7b9b7f5c47fec95b12021882bcf1f29f421cfd9
Xynkt_99 <pawan.dogra99@gmail.com>
2020-05-09 17:22:25 +0530

    Added loss function and GMM

--- utils/loss.py
+++ utils/loss.py
@@ -2,3 +2,13 @@ import torch
 import torch.nn.functional as F
 from utils import gaussian_distribution
 
+def MDN_loss(y,pi,mu,sigma):
+	g=gaussian_distribution(y,mu,sigma)
+	assert(g.shape==p.shape)
+	prob=pi*g
+	prob=torch.prod(prob,1)
+	nll=-torch.log(torch.sum(prob,dim=1))
+	return torch.mean(nll)
+
+
+
